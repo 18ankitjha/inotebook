650ac97b40179b2267fa4c2ca6b66495b5d0e24b
ANKIT JHA <ankitjha01aj@gmail.com>
2022-09-22 01:14:00 +0530

    deleted a line

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,4 +1,4 @@
-const { default: userEvent } = require("@testing-library/user-event");
+
 const express = require("express");
 const router = express.Router();
 const User = require('../models/User');
